2740aba8eff181d89553c877016a5c89aeb54ed1
Bryce Wein <bryce.wein@gmail.com>
2022-04-26 12:12:44 -0400

    Feat: add methods for overstocked items and total inventory

--- lib/event.rb
+++ lib/event.rb
@@ -22,4 +22,25 @@ class Event
     items = @food_trucks.map { |truck| truck.inventory.keys }.flatten.uniq
     items.sort_by { |item| item.name }
   end
+
+  def overstocked_items
+    total_inventory.select do |_item, info|
+      info[:quantity] > 50 && info[:food_trucks].length > 1
+    end.keys
+  end
+
+  def total_inventory
+    items = {}
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, amount|
+        if items[item].nil?
+          items[item] = { quantity: amount, food_trucks: [truck] }
+        else
+          items[item][:quantity] += amount
+          items[item][:food_trucks] << truck
+        end
+      end
+    end
+    items
+  end
 end
