23963eb9b56e313b06f8b225b74ccbe33bfd690d
Bryce Wein <bryce.wein@gmail.com>
2022-04-26 11:49:58 -0400

    Test: add test for sorted item list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -56,4 +56,26 @@ describe Event do
     expect(event.food_trucks_that_sell(item1)).to eq(expected)
     expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
   end
+
+  it 'can return a list of all items sorted alphabetically by name' do
+    event = Event.new('South Pearl Street Farmers Market')
+    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+    item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
+    item3 = Item.new({ name: 'Peach-Raspberry Nice Cream', price: '$5.30' })
+    item4 = Item.new({ name: 'Banana Nice Cream', price: '$4.25' })
+    food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new('Palisade Peach Shack')
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected = [item2, item4, item1, item3]
+    expect(event.sorted_item_list).to eq(expected)
+  end
 end
