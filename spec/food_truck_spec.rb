fbe2424919484b6410d81a9475beb5312322552c
Bryce Wein <bryce.wein@gmail.com>
2022-04-26 11:10:38 -0400

    Test: add initilization test

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/item'
+require './lib/food_truck'
+
+describe FoodTruck do
+  it 'exists with attributes' do
+    food_truck = FoodTruck.new('Rocky Mountain Pies')
+    expect(food_truck).to be_a(FoodTruck)
+    expect(food_truck.name).to eq('Rocky Mountain Pies')
+    expect(food_truck.inventory).to eq({})
+  end
+end
