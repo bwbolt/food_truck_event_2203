66836e0661786bbc2b62be88264fa9d597d0776b
Bryce Wein <bryce.wein@gmail.com>
2022-04-26 11:36:33 -0400

    Feat: add trucks that sell method

--- lib/event.rb
+++ lib/event.rb
@@ -13,4 +13,8 @@ class Event
   def food_truck_names
     @food_trucks.map { |truck| truck.name }
   end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.select { |truck| truck.inventory.keys.include?(item) }
+  end
 end
