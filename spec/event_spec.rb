3e18cc48ef3cab0599f22b4e7dd537cdb94e6f7e
Bryce Wein <bryce.wein@gmail.com>
2022-04-26 11:30:24 -0400

    Test: add test for getting food truck names

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -22,4 +22,16 @@ describe Event do
     event.add_food_truck(food_truck3)
     expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
   end
+
+  it 'can give an array of food truck names that have been added' do
+    event = Event.new('South Pearl Street Farmers Market')
+    food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
+    food_truck3 = FoodTruck.new('Palisade Peach Shack')
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected = ['Rocky Mountain Pies', 'Ba-Nom-a-Nom', 'Palisade Peach Shack']
+    expect(event.food_truck_names).to eq(expected)
+  end
 end
