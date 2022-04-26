b9c424161c411d32d0778e9dee2ecc9ed5607989
Bryce Wein <bryce.wein@gmail.com>
2022-04-26 11:28:09 -0400

    Test: add test of adding food trucks

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -10,4 +10,16 @@ describe Event do
     expect(event.name).to eq('South Pearl Street Farmers Market')
     expect(event.food_trucks).to eq([])
   end
+
+  it 'can add foot trucks' do
+    event = Event.new('South Pearl Street Farmers Market')
+    food_truck1 = FoodTruck.new('Rocky Mountain Pies')
+    food_truck2 = FoodTruck.new('Ba-Nom-a-Nom')
+    food_truck3 = FoodTruck.new('Palisade Peach Shack')
+    expect(event.food_trucks).to eq([])
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.food_trucks).to eq([food_truck1, food_truck2, food_truck3])
+  end
 end
