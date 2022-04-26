62e89e24de5ff1435a3ccfb50478f0a92865faf7
Bryce Wein <bryce.wein@gmail.com>
2022-04-26 12:49:35 -0400

    Refactor: add helper method sell by truck to break up code

--- lib/event.rb
+++ lib/event.rb
@@ -51,19 +51,23 @@ class Event
     if total_inventory[item].nil? || total_inventory[item][:quantity] < amount
       false
     else
-      total_left = amount
-      until total_left == 0
-        @food_trucks.each do |truck|
-          if truck.inventory[item] <= total_left
-            total_left -= truck.inventory[item]
-            truck.sell_item(item, truck.inventory[item])
-          else
-            truck.sell_item(item, total_left)
-            total_left -= total_left
-          end
+      sell_by_truck(item, amount)
+      true
+    end
+  end
+
+  def sell_by_truck(item, amount)
+    total_left = amount
+    until total_left == 0
+      @food_trucks.each do |truck|
+        if truck.inventory[item] <= total_left
+          total_left -= truck.inventory[item]
+          truck.sell_item(item, truck.inventory[item])
+        else
+          truck.sell_item(item, total_left)
+          total_left -= total_left
         end
       end
-      true
     end
   end
 end
